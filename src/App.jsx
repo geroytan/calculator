d5e764d5e3bb5fd0e7d967aa46e04561a076d52d
geroytan <gry.tan51@gmail.com>
2024-05-28 18:06:41 +0800

    Third Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { Page, Block, Button } from 'framework7-react';
-import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
+import { Page, View, Block, Button } from "framework7-react";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
 
 function App() {
   const [display, setDisplay] = useState("");
@@ -31,22 +31,22 @@ function App() {
       setDisplay("Error");
       setCalculated(false);
     }
-  }
+  };
 
   const toggleSign = () => {
     setDisplay((prevDisplay) => {
       if (prevDisplay) {
-        if (prevDisplay.charAt(0) === '-') {
+        if (prevDisplay.charAt(0) === "-") {
           return prevDisplay.substring(1);
         } else {
-          return '-' + prevDisplay;
+          return "-" + prevDisplay;
         }
       }
       return prevDisplay;
     });
   };
 
-  const calculatePercentage = () => {  
+  const calculatePercentage = () => {
     setDisplay((prevDisplay) => {
       if (prevDisplay) {
         try {
@@ -58,34 +58,77 @@ function App() {
       }
       return prevDisplay;
     });
-  }
+  };
 
   return (
     <Page>
-      <Block className="calculator">
-        <div className="display">{display}</div>
-        <div className="grid-container">
-          <Button className="button2" onClick={clearDisplay}>C</Button>
-          <Button className="button2" onClick={toggleSign}>+/-</Button>
-          <Button className="button2" onClick={calculatePercentage}>%</Button>
-          <Button className="button1" onClick={() => appendToDisplay('/')}>/</Button>
-          <Button className="button" onClick={() => appendToDisplay('7')}>7</Button>
-          <Button className="button" onClick={() => appendToDisplay('8')}>8</Button>
-          <Button className="button" onClick={() => appendToDisplay('9')}>9</Button>
-          <Button className="button1" onClick={() => appendToDisplay('*')}>*</Button>
-          <Button className="button" onClick={() => appendToDisplay('4')}>4</Button>
-          <Button className="button" onClick={() => appendToDisplay('5')}>5</Button>
-          <Button className="button" onClick={() => appendToDisplay('6')}>6</Button>
-          <Button className="button1" onClick={() => appendToDisplay('-')}>-</Button>
-          <Button className="button" onClick={() => appendToDisplay('1')}>1</Button>
-          <Button className="button" onClick={() => appendToDisplay('2')}>2</Button>
-          <Button className="button" onClick={() => appendToDisplay('3')}>3</Button>
-          <Button className="button1" onClick={() => appendToDisplay('+')}>+</Button>
-          <Button className="button button-zero" onClick={() => appendToDisplay('0')}>0</Button>
-          <Button className="button" onClick={() => appendToDisplay('.')}>.</Button>
-          <Button className="button1" onClick={calculate}>=</Button>
-        </div>
-      </Block>
+      <View>
+        <Block className="calculator">
+          <div className="display">{display}</div>
+          <div className="grid-container">
+            <Button className="button2" onClick={clearDisplay}>
+              C
+            </Button>
+            <Button className="button2" onClick={toggleSign}>
+              +/-
+            </Button>
+            <Button className="button2" onClick={calculatePercentage}>
+              %
+            </Button>
+            <Button className="button1" onClick={() => appendToDisplay("/")}>
+              /
+            </Button>
+            <Button className="button" onClick={() => appendToDisplay("7")}>
+              7
+            </Button>
+            <Button className="button" onClick={() => appendToDisplay("8")}>
+              8
+            </Button>
+            <Button className="button" onClick={() => appendToDisplay("9")}>
+              9
+            </Button>
+            <Button className="button1" onClick={() => appendToDisplay("*")}>
+              *
+            </Button>
+            <Button className="button" onClick={() => appendToDisplay("4")}>
+              4
+            </Button>
+            <Button className="button" onClick={() => appendToDisplay("5")}>
+              5
+            </Button>
+            <Button className="button" onClick={() => appendToDisplay("6")}>
+              6
+            </Button>
+            <Button className="button1" onClick={() => appendToDisplay("-")}>
+              -
+            </Button>
+            <Button className="button" onClick={() => appendToDisplay("1")}>
+              1
+            </Button>
+            <Button className="button" onClick={() => appendToDisplay("2")}>
+              2
+            </Button>
+            <Button className="button" onClick={() => appendToDisplay("3")}>
+              3
+            </Button>
+            <Button className="button1" onClick={() => appendToDisplay("+")}>
+              +
+            </Button>
+            <Button
+              className="button button-zero"
+              onClick={() => appendToDisplay("0")}
+            >
+              0
+            </Button>
+            <Button className="button" onClick={() => appendToDisplay(".")}>
+              .
+            </Button>
+            <Button className="button1" onClick={calculate}>
+              =
+            </Button>
+          </div>
+        </Block>
+      </View>
     </Page>
   );
 }
